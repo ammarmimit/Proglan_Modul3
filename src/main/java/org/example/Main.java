335e9c7f525bc826f57adcfb3ba35c165e65c1f5
ammarmimit <fadgham2002@gmail.com>
2025-10-20 10:51:59 +0800

    tesbaru

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -2,6 +2,6 @@ package org.example;
 
 public class Main {
     public static void main(String[] args) {
-        System.out.println("saya pengen bab");
+        System.out.println("NAMA SAYA FADGHAM AMMAR HIBATULLAH NIM 202310370311467");
     }
 }
